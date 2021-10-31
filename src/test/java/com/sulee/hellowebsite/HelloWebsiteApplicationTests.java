c5bc9e09eda2e72abe5d6849c4eddb9547d0e523
suleesulee <seonguk9109@gmail.com>
2021-10-31 10:51:25 +0900

    Initial commit

---
+++ src/test/java/com/sulee/hellowebsite/HelloWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sulee.hellowebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
